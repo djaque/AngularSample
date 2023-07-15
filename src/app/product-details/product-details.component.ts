61a4ada44122f578c6bed003af31c5b9b4fd6174
djaque <djaque@gmail.com>
2023-07-15 14:28:59 -0400

    Cart view and working adding items from detail view - 4 files modified, 6 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {   
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     // Get the id from the current route.
     const routerParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routerParams.get('productId'))
-
+    const productIdFromRoute = Number(routerParams.get('productId'));
 
     // Find the productId from the list of products
-    this.product = products.find(product => product.id === productIdFromRoute)
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart');
   }
 }
