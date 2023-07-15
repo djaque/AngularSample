fcb71b773d0daec1e5b95c277084011846f30105
PUTYOURNAME <YOUR@EMAIL>
2023-07-15 17:11:06 -0400

    Add form and checkout process sample

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { CartService } from '../cart.service';
+import { Component } from "@angular/core";
+import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css'],
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"],
 })
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn("Your order has been submitted", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
